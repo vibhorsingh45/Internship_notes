32b6a1fd42180ff5a24ae10c2c836b9aff81711c
Vibhor Singh <vibhorsingh0045@gmail.com>
2025-08-02 09:39:45 +0530

    Python Basics
    FIrst day notes of Internship training program uploaded in which python language basics are covered.

---
+++ Day1_internship.py
@@ -0,0 +1,12 @@
+Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> name = "Vibhor"
+>>> sname = "Singh"
+>>> address_is = '''1/158, Rajnikhand
+...             Shardanagar,
+...             Lucknow.
+...             '''
+>>> cityName = "Vibhor's city name is lucknow"
+>>> print = (name, sname, address_is, cityName)
+
+
